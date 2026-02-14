b8dfa6ef9b4b9f1b1630eb67d69f4390256a8caf
CristianSault <25013012@saultcollege.ca>
2026-02-14 01:39:03 -0500

    Completed lab-3

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,8 +1,10 @@
 package core;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * The base of any card game. Defines each Card's rank and suit
+ * @param rank
+ * @param suit
+ */
 public record Card(Rank rank, Suit suit) {
 
     static public enum Suit {
@@ -20,8 +22,9 @@ public record Card(Rank rank, Suit suit) {
     }
 
     static public enum Rank {
-        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10),
-        JACK(10), QUEEN(10), KING(10), ACE(11);
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+        EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10),
+        ACE(11);
 
         private final int value;
 
@@ -34,4 +37,31 @@ public record Card(Rank rank, Suit suit) {
         }
     }
 
+    /**
+     * Overwritten method for @toString()
+     * @return the String representation for this Card
+     */
+    public String toString() {
+        return switch (rank) {
+            case ACE -> "A%s".formatted(suit().symbol());
+            case JACK -> "J%s".formatted(suit().symbol());
+            case QUEEN -> "Q%s".formatted(suit().symbol());
+            case KING -> "K%s".formatted(suit().symbol());
+            default -> "%d%s".formatted(rank().value(), suit().symbol());
+        };
+    }
+
+    /**
+     * Overwritten method for @equals()
+     * @param other the reference object with which to compare
+     * @return whether @other is equal to this object
+     */
+    public boolean equals(Object other){
+        if (other == null || other.getClass() != this.getClass()) {
+            return false;
+        }
+        Card otherCard =  (Card)other;
+        return suit.equals(otherCard.suit)
+                && rank.equals(otherCard.rank);
+    }
 }
--- src/main/java/core/Player.java
+++ src/main/java/core/Player.java
@@ -1,15 +1,21 @@
 package core;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 /**
- *
+ * The main actors in the card game
  */
 public class Player {
-    private String name;
-    private CardStack hand; // containing only 26 cards
+    private final String name;
+    private final CardStack hand; // containing only 26 cards
     private int score;
 
+    /**
+     * Default Constructor of the @Player class
+     * @param name the String representing the player's name
+     * @param hand the CardStack representing the player's hand
+     */
     public Player(String name, CardStack hand){
         this.name = name;
         this.hand = hand;   // the Player's hand must be shuffled before constructing.
@@ -24,42 +30,63 @@ public class Player {
         score = 0;
     }
 
-    public void rename(String newName){
-        name = newName;
-    }
-
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public int getScore() {
         return score;
     }
 
+    /**
+     * Simple boolean method to check whether a Player's hand is empty or not.
+     * @return true if deck has cards, false otherwise
+     */
+    public boolean hasCards() {
+        return hand.remainingCards() != 0;
+    }
+
+    /**
+     * Wrapper method around the @shuffle() method in the @CardStack class
+     */
     public void shuffleHand(){
         hand.shuffle();
     }
 
-    public Card playCard(){
+    /**
+     * Wrapper method around the @drawOne() method in the @CardStack class
+     * @return the top Card of the player's hand
+     * @throws NoSuchElementException a throwable from @ArrayList's @removeFirst() method
+     */
+    public Card playCard() throws NoSuchElementException {
         return hand.drawOne();
     }
 
-    //Overwritten methods
+    /**
+     * Overwritten method for @toString()
+     * @return the String representation for this Player
+     */
     public String toString(){
         return "Player: %s\nScore: %d\n".formatted(name, score);
     }
 
+    /**
+     * Overwritten method for @equals()
+     * @param other the reference object with which to compare
+     * @return whether @other is equal to this object
+     */
     public boolean equals(Object other){
         if (other == null || other.getClass() != this.getClass()) {
             return false;
         }
-
         Player otherPlayer = (Player)other;
         return name.equals(otherPlayer.name)
                 && hand.equals(otherPlayer.hand)
                 && score == otherPlayer.score;
     }
 
+    /**
+     * Overwritten method for @hashCode()
+     * @return a hashed version of this object
+     */
     public int hashCode(){
         return Objects.hash(name, hand, score);
     }
